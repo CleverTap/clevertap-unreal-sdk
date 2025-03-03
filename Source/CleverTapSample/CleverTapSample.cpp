91942eb11ede142cb3fb94bb7daca2aeea3682b4
Dan Baker <dbaker@bitwisealchemy.com>
2025-03-03 10:44:33 -0600

    Initial plugin and sample project created from blank templates

---
+++ Source/CleverTapSample/CleverTapSample.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CleverTapSample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CleverTapSample, "CleverTapSample" );
---
+++ Source/CleverTapSample/CleverTapSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CleverTapSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLEVERTAPSAMPLE_API ACleverTapSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
