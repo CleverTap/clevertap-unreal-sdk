eba9b44ca364cc79c08bc7ccd642521afa2a153b
Dan Baker <dbaker@bitwisealchemy.com>
2025-04-17 12:57:18 -0500

    Ignoring unused parameters in FNullCleverTapInstance method implementations

--- Plugins/CleverTap/Source/CleverTap/Private/NullCleverTapInstance.cpp
+++ Plugins/CleverTap/Source/CleverTap/Private/NullCleverTapInstance.cpp
@@ -1,40 +1,81 @@
 // Copyright CleverTap All Rights Reserved.
 #include "NullCleverTapInstance.h"
 
+#include "CleverTapUtilities.h"
+
 FString FNullCleverTapInstance::GetCleverTapId()
 {
 	return FString{};
 }
 
-void FNullCleverTapInstance::OnUserLogin(const FCleverTapProperties& Profile) {}
+void FNullCleverTapInstance::OnUserLogin(const FCleverTapProperties& Profile)
+{
+	CleverTapSDK::Ignore(Profile);
+}
 
-void FNullCleverTapInstance::OnUserLogin(const FCleverTapProperties& Profile, const FString& CleverTapId) {}
+void FNullCleverTapInstance::OnUserLogin(const FCleverTapProperties& Profile, const FString& CleverTapId)
+{
+	CleverTapSDK::Ignore(Profile, CleverTapId);
+}
 
-void FNullCleverTapInstance::PushProfile(const FCleverTapProperties& Profile) {}
+void FNullCleverTapInstance::PushProfile(const FCleverTapProperties& Profile)
+{
+	CleverTapSDK::Ignore(Profile);
+}
 
-void FNullCleverTapInstance::PushEvent(const FString& EventName) {}
+void FNullCleverTapInstance::PushEvent(const FString& EventName)
+{
+	CleverTapSDK::Ignore(EventName);
+}
 
-void FNullCleverTapInstance::PushEvent(const FString& EventName, const FCleverTapProperties& Actions) {}
+void FNullCleverTapInstance::PushEvent(const FString& EventName, const FCleverTapProperties& Actions)
+{
+	CleverTapSDK::Ignore(EventName, Actions);
+}
 
 void FNullCleverTapInstance::PushChargedEvent(
 	const FCleverTapProperties& ChargeDetails, const TArray<FCleverTapProperties>& Items)
 {
+	CleverTapSDK::Ignore(ChargeDetails, Items);
 }
 
-void FNullCleverTapInstance::DecrementValue(const FString& Key, int Amount) {}
-void FNullCleverTapInstance::DecrementValue(const FString& Key, double Amount) {}
+void FNullCleverTapInstance::DecrementValue(const FString& Key, int Amount)
+{
+	CleverTapSDK::Ignore(Key, Amount);
+}
 
-void FNullCleverTapInstance::IncrementValue(const FString& Key, int Amount) {}
-void FNullCleverTapInstance::IncrementValue(const FString& Key, double Amount) {}
+void FNullCleverTapInstance::DecrementValue(const FString& Key, double Amount)
+{
+	CleverTapSDK::Ignore(Key, Amount);
+}
+
+void FNullCleverTapInstance::IncrementValue(const FString& Key, int Amount)
+{
+	CleverTapSDK::Ignore(Key, Amount);
+}
+
+void FNullCleverTapInstance::IncrementValue(const FString& Key, double Amount)
+{
+	CleverTapSDK::Ignore(Key, Amount);
+}
 
 bool FNullCleverTapInstance::IsPushPermissionGranted()
 {
 	return false;
 }
 
-void FNullCleverTapInstance::PromptForPushPermission(bool bFallbackToSettings) {}
-void FNullCleverTapInstance::PromptForPushPermission(const FCleverTapPushPrimerAlertConfig& PushPrimerAlertConfig) {}
+void FNullCleverTapInstance::PromptForPushPermission(bool bFallbackToSettings)
+{
+	CleverTapSDK::Ignore(bFallbackToSettings);
+}
+
+void FNullCleverTapInstance::PromptForPushPermission(const FCleverTapPushPrimerAlertConfig& PushPrimerAlertConfig)
+{
+	CleverTapSDK::Ignore(PushPrimerAlertConfig);
+}
+
 void FNullCleverTapInstance::PromptForPushPermission(
 	const FCleverTapPushPrimerHalfInterstitialConfig& PushPrimerHalfInterstitialConfig)
 {
+	CleverTapSDK::Ignore(PushPrimerHalfInterstitialConfig);
 }
