208ab7f8291eb864673da4b474da5ee03bae8d39
simonbullen <93950597+simonbullen@users.noreply.github.com>
2025-06-26 12:38:34 +1000

    HtmlBannerInAppDisplayProvider - custom-html header & footer notifications (#24)
    Adds HtmlBannerInAppDisplayProvider to handle the display of custom-html header & footer banner-notifications.
    The normal CleverTap implementation relies on a FragmentActivity which unreal can't easily support.
    
    This functionality requires adding the CustomInAppDisplayProvider interface to the clevertap android sdk.
    CleverTap/clevertap-android-sdk#839
    
    This PR includes (and defaults to) a prebuilt clevertap-android-sdk.aar with this pre-release functionality included.
    The AAR will be removed when the maven version has the needed features.

---
+++ Plugins/CleverTap/Source/CleverTap/Private/Android/Java/TemporaryOverlayStubs/HtmlBannerInAppDisplayProvider.java
@@ -0,0 +1,18 @@
+package com.clevertap.android.unreal;
+
+// TODO: Remove this stub file (and the UPL copy step) when the Maven version of clevertap-android-sdk 
+//       includes the CustomInAppDisplayProvider interface
+//
+// CleverTap_Android_UPL.xml copies this file over the real HtmlBannerInAppDisplayProvider.java 
+// when bAndroidEnableHtmlBannerInAppDisplayProvider is false.
+//
+// This is currently required when using the published clevertap-android-sdk, 
+// as the real HtmlBannerInAppDisplayProvider.java does not compile without 
+// the new CustomInAppDisplayProvider interface definition in the sdk.  
+//
+
+@Deprecated
+public class HtmlBannerInAppDisplayProvider {
+    // Stub class to satisfy build requirements when the provider interface is unavailable.
+}
+
